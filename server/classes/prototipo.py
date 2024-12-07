d427ebabdab47c485efc0b92deac2272012a67ba
Ítalo Nunes <94195116+ItaloNunes204@users.noreply.github.com>
2024-12-07 16:47:05 -0300

    0.14.0
    criação da classe de prototipo

---
+++ server/classes/prototipo.py
@@ -0,0 +1,9 @@
+class Prototipo:
+    def __init__(self, id, nome, ano_fabricacao,status,peso,temporada):
+        self.id = id
+        self.nome = nome
+        self.ano_fabricacao = ano_fabricacao
+        self.status = status
+        self.peso = peso
+        self.temporada = temporada
+        
