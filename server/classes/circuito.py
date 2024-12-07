f360bc0b5e0ec5fa2b0d26ea8e745b4c2ee0b468
Ítalo Nunes <94195116+ItaloNunes204@users.noreply.github.com>
2024-12-07 18:11:13 -0300

    0.18.0
    criação da classe circuito

---
+++ server/classes/circuito.py
@@ -0,0 +1,9 @@
+class Circuito:
+    def __init__(self, id_circuito, nome, tempo_descolcamento, KM, curvas, cones, local):
+        self.id_circuito = id_circuito
+        self.nome = nome
+        self.tempo_descolcamento = tempo_descolcamento
+        self.KM = KM
+        self.curvas = curvas
+        self.cones = cones
+        self.local = local
