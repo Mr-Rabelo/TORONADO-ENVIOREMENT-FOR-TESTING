4476ee9c0c89d804a67968664c953f17ea9744de
Ítalo Nunes <94195116+ItaloNunes204@users.noreply.github.com>
2024-12-07 15:45:03 -0300

    0.7.0
    implementação da classe membro

---
+++ server/classes/membros.py
@@ -0,0 +1,6 @@
+class Membros:
+    def __init__(self, email, senha, nome, subgrupo) ->None:
+        self.email = email
+        self.senha = senha
+        self.nome = nome
+        self.subgrupo = subgrupo
