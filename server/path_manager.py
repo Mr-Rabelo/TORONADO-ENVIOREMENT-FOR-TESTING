052ff4a7431540b4a8d0c004a79120351bb0e557
Ítalo Nunes <94195116+ItaloNunes204@users.noreply.github.com>
2024-12-16 20:32:59 -0300

    0.19.0
    função para pegar o caminho da pasta upload

---
+++ server/path_manager.py
@@ -0,0 +1,10 @@
+import os
+
+def join_path(path,folder_file):
+    return os.path.join(path,folder_file)
+
+def get_path():
+    return os.getcwd()
+
+def get_upload_path():
+    return join_path(join_path(get_path(),"server"),"upload")
