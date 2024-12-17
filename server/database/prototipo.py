2701bc1ed0b6d424a96a5cc37435f6e635fc18aa
Ítalo Nunes <94195116+ItaloNunes204@users.noreply.github.com>
2024-12-17 03:14:22 -0300

    0.30.0
    função modificar informações do banco de dados

--- server/database/prototipo.py
+++ server/database/prototipo.py
@@ -65,3 +65,19 @@ def get_prototipo(id_prototipos):
         return verificador, var_login
     else:
         return verificador, None
+
+def modifica(prototipo):
+    comando = """UPDATE {} SET nome = \'{}\', ano_fabricacao = \'{}\', status = \'{}\', peso = \'{}\', temporada = \'{}\' WHERE id_prototipo = \'{}\'""".format(TABLE,prototipo.nome, prototipo.ano_fabricacao, prototipo.status, prototipo.peso, prototipo.temporada, prototipo.id)
+    verificador, cursor, con = connection.connect_to_db()
+    if verificador == True:
+        try:
+            cursor.execute(comando)
+            con.commit()
+            var_login = True
+        except Error as e:
+            var_login = False
+            send_email(e)
+        connection.close_connect_to_bd(cursor, con)
+        return verificador, var_login
+    else:
+        return verificador, None
