684dc0f40c544fc6b0cefa1cccb5250d87fdcb17
Ítalo Nunes <94195116+ItaloNunes204@users.noreply.github.com>
2024-12-16 22:51:53 -0300

    0.23.0
    função para pegar as informações de um usuário

--- server/database/membros.py
+++ server/database/membros.py
@@ -55,5 +55,26 @@ def get_membros():
     else:
         return verificador, None
 
+def get_membro(email):
+    comando = ("SELECT * FROM {} WHERE email = '{}'".format(TABLE, email)) # comando sql 
+    verificador, cursor, con = connection.connect_to_db() # coleta as informações para a conexão com o banco 
+    if verificador == True:
+        try:
+            # tenta executar o comando 
+            cursor.execute(comando) 
+            linhas = cursor.fetchall()
+            # verifica a informação
+            saida = [] 
+            for linha in linhas:
+                saida.append(membros.Membros(linha[1], None, linha[0], linha[2]))
+            var_login = saida
+        except Error as e: # 
+            verificador = False
+            send_email(e)
+        # finaliza a conexão com o banco 
+        connection.close_connect_to_bd(cursor,con)
+        return verificador, var_login
+    else:
+        return verificador, None
 def creat_membro(membro):
     pass
