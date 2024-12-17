83452475b5c673469d4777cf1d06b707d9fa4d44
Ítalo Nunes <94195116+ItaloNunes204@users.noreply.github.com>
2024-12-16 21:02:52 -0300

    0.21.0
    função para coletar o caminho da pasta templates

--- server/path_manager.py
+++ server/path_manager.py
@@ -8,3 +8,6 @@ def get_path():
 
 def get_upload_path():
     return join_path(join_path(get_path(),"server"),"upload")
+
+def get_templates_path():
+    return join_path(join_path(get_path(),"server"),"templates")
