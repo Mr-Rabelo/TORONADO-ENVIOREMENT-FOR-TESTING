b14ab923ef538a756a694af3e82ea772886e0ab4
Ítalo Nunes <94195116+ItaloNunes204@users.noreply.github.com>
2024-12-16 22:20:54 -0300

    0.22.0
    função para coletar todas os registros de membros

--- server/database/membros.py
+++ server/database/membros.py
@@ -5,15 +5,15 @@ root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(root_path)
 
 from mysql.connector import Error
-from db_connection import connect_to_db, close_connect_to_bd
 
+from database import connection
 from error_reporter import send_email
-
+from server.classes import membros
 TABLE = "TEFT.membro"
 
 def login(email, senha):
     comando = ("SELECT * FROM {} WHERE email = '{}' and senha = '{}'".format(TABLE, email, senha)) # comando sql 
-    verificador, cursor, con = connect_to_db() # coleta as informações para a conexão com o banco 
+    verificador, cursor, con = connection.connect_to_db() # coleta as informações para a conexão com o banco 
     if verificador == True:
         try:
             # tenta executar o comando 
@@ -28,8 +28,32 @@ def login(email, senha):
             verificador = False
             send_email(e)
         # finaliza a conexão com o banco 
-        close_connect_to_bd(cursor,con)
+        connection.close_connect_to_bd(cursor,con)
         return verificador, var_login
     else:
         return verificador, None
 
+def get_membros():
+    comando = ("SELECT * FROM {} ".format(TABLE)) # comando sql 
+    verificador, cursor, con = connection.connect_to_db() # coleta as informações para a conexão com o banco 
+    if verificador == True:
+        try:
+            # tenta executar o comando 
+            cursor.execute(comando) 
+            linhas = cursor.fetchall()
+            # verifica a informação
+            saida = [] 
+            for linha in linhas:
+                saida.append(membros.Membros(linha[1], None, linha[0], linha[2]))
+            var_login = saida
+        except Error as e: # 
+            verificador = False
+            send_email(e)
+        # finaliza a conexão com o banco 
+        connection.close_connect_to_bd(cursor,con)
+        return verificador, var_login
+    else:
+        return verificador, None
+
+def creat_membro(membro):
+    pass
