28e98ee30a9db514e276826d6cf7d2f8a438cdbb
Ítalo Nunes <94195116+ItaloNunes204@users.noreply.github.com>
2024-12-17 03:18:04 -0300

    0.31.0
    função para apagar registros

--- server/database/prototipo.py
+++ server/database/prototipo.py
@@ -81,3 +81,19 @@ def modifica(prototipo):
         return verificador, var_login
     else:
         return verificador, None
+
+def apagar(prototipo):
+    comando = """DELETE FROM {} WHERE id_prototipo = \'{}\'""".format(TABLE, prototipo.id)
+    verificador, cursor, con = connection.connect_to_db()
+    if verificador == True:
+        try:
+            cursor.execute(comando)
+            con.commit()
+            var_login = True
+        except Error as e:
+            var_login = False
+            send_email(e)
+            connection.close_connect_to_bd(cursor,con)
+        return verificador, var_login
+    else:
+        return verificador, None
