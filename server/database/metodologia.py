8a5488adb5ab9b88ec32e1eddcc7b139c1c1175a
Ítalo Nunes <94195116+ItaloNunes204@users.noreply.github.com>
2024-12-18 02:28:23 -0300

    0.36.0
    implementação da função de criação de metodologia

---
+++ server/database/metodologia.py
@@ -0,0 +1,29 @@
+import sys
+import os
+
+root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(root_path)
+
+from mysql.connector import Error
+
+from database import connection
+from error_reporter import send_email
+from server.classes import metodologia
+
+TABLE = "TEFT.metodologia"
+
+def creat_metodologia(metodologia):
+    comando = """INSERT INTO {} (objetivo, N_pessoas, subgrupo, procedimento, N_voltas) VALUE(\'{}\',\'{}\',\'{}\',\'{}\',\'{}\')""".format(TABLE, metodologia.objetivo, metodologia.N_pessoas, metodologia.subgrupo, metodologia.procedimento, metodologia.N_voltas)
+    verificador, cursor, con = connection.connect_to_db()
+    if verificador == True:
+        try:
+            cursor.execute(comando)
+            con.commit()
+            var_login = True
+        except Error as e:
+            var_login = False
+            send_email(e)
+        connection.close_connect_to_bd(cursor, con)
+        return verificador, var_login
+    else:
+        return verificador, None
