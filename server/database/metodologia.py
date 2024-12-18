4af09c736e4afbb85c785dfbdc317ecc7d31bac1
Ítalo Nunes <94195116+ItaloNunes204@users.noreply.github.com>
2024-12-18 02:37:27 -0300

    0.39.0
    função de busca de informações de um registro

--- server/database/metodologia.py
+++ server/database/metodologia.py
@@ -61,7 +61,29 @@ def modificar(metodologia):
         return verificador, None
 
 def get_metodologias():
-    comando = ("SELECT * FROM {} ".format(TABLE)) # comando sql 
+    comando = "SELECT * FROM {} ".format(TABLE) # comando sql 
+    verificador, cursor, con = connection.connect_to_db() # coleta as informações para a conexão com o banco 
+    if verificador == True:
+        try:
+            # tenta executar o comando 
+            cursor.execute(comando) 
+            linhas = cursor.fetchall()
+            # verifica a informação
+            saida = [] 
+            for linha in linhas:
+                saida.append(metodologia.Metodologia(linha[0], linha[1], linha[2], linha[3], linha[4], linha[5]))
+            var_login = saida
+        except Error as e: # 
+            verificador = False
+            send_email(e)
+        # finaliza a conexão com o banco 
+        connection.close_connect_to_bd(cursor,con)
+        return verificador, var_login
+    else:
+        return verificador, None
+
+def get_metodologia(id_metodologia):
+    comando = "SELECT * FROM {} WHERE id_metodologia = \'{}\'".format(TABLE,id_metodologia) # comando sql 
     verificador, cursor, con = connection.connect_to_db() # coleta as informações para a conexão com o banco 
     if verificador == True:
         try:
