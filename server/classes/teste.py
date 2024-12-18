6bd023da0e1648ca417f3f9556f96340e44f4b59
Ítalo Nunes <94195116+ItaloNunes204@users.noreply.github.com>
2024-12-18 03:17:37 -0300

    0.41.0
    implementação da classe teste

---
+++ server/classes/teste.py
@@ -0,0 +1,12 @@
+class Teste:
+    def __init__(self, N_teste, pilotos, id_objetivos, N_voltas, inicio, fim, almoco, data, id_prototipo, id_circuito):
+        self.N_teste = N_teste
+        self.pilotos = pilotos
+        self.id_objetivos = id_objetivos
+        self.N_voltas = N_voltas
+        self.inicio = inicio
+        self.fim = fim
+        self.almoco = almoco
+        self.data = data
+        self.id_prototipo = id_prototipo
+        self.id_circuito = id_circuito
