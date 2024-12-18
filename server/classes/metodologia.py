d3751d9cb7330475085625630fa88ee7b2122be2
Ítalo Nunes <94195116+ItaloNunes204@users.noreply.github.com>
2024-12-18 02:39:05 -0300

    0.40.0
    implementação da função que modifica as informações da classe metodologia

--- server/classes/metodologia.py
+++ server/classes/metodologia.py
@@ -5,4 +5,11 @@ class Metodologia:
         self.N_pessoas = N_pessoas
         self.subgrupo = subgrupo
         self.procedimento = procedimento
-        self.N_voltas = N_pessoas
+        self.N_voltas = N_voltas
+    
+    def modificar(self, objetivo, N_pessoas, subgrupo, procedimento, N_voltas):
+        self.objetivo = objetivo
+        self.N_pessoas = N_pessoas
+        self.subgrupo = subgrupo
+        self.procedimento = procedimento
+        self.N_voltas = N_voltas
