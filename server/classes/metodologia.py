0499a88d6430af6281c6dd193a73f6afdcd5abf3
Ítalo Nunes <94195116+ItaloNunes204@users.noreply.github.com>
2024-12-18 02:18:54 -0300

    0.35.0
    implementação da classe metodologia

---
+++ server/classes/metodologia.py
@@ -0,0 +1,8 @@
+class Metodologia:
+    def __init__(self, id_metodologia, objetivo, N_pessoas, subgrupo, procedimento, N_voltas):
+        self.id_metodologia = id_metodologia
+        self.objetivo = objetivo
+        self.N_pessoas = N_pessoas
+        self.subgrupo = subgrupo
+        self.procedimento = procedimento
+        self.N_voltas = N_pessoas
