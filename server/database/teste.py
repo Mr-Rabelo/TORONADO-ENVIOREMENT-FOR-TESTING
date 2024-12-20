865e0834514c445a018d7f41276bb9e438800815
Ítalo Nunes <94195116+ItaloNunes204@users.noreply.github.com>
2024-12-20 01:06:33 -0300

    0.45.0
    função para apagar um teste

--- server/database/teste.py
+++ server/database/teste.py
@@ -71,3 +71,19 @@ def creat_teste(teste):
         return verificador, var_login
     else:
         return verificador, None
+
+def apagar(teste):
+    comando = """DELETE FROM {} WHERE N_teste = \'{}\'""".format(TABLE, teste.N_teste)
+    verificador, cursor, con = connection.connect_to_db()
+    if verificador == True:
+        try:
+            cursor.execute(comando)
+            con.commit()
+            var_login = True
+        except Error as e:
+            var_login = False
+            send_email(e)
+        connection.close_connect_to_bd(cursor, con)
+        return verificador, var_login
+    else:
+        return verificador, None
